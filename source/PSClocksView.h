5f3b8456ca8f3962f7b0e4ceb97ba1c2252f0445
Alexandre Aybes <aaybes@strava.com>
2012-12-30 09:10:47 -0800

    Reorganizing and adding a project
    Removing old clocks directory

---
+++ source/PSClocksView.h
@@ -0,0 +1,13 @@
+//
+//  clocksView.h
+//  clocks
+//
+//  Created by Alexandre Aybes on 12/30/12.
+//  Copyright (c) 2012 Alexandre Aybes. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface PSClocksView : ScreenSaverView
+
+@end
---
+++ source/PSClocksView.m
@@ -0,0 +1,52 @@
+//
+//  clocksView.m
+//  clocks
+//
+//  Created by Alexandre Aybes on 12/30/12.
+//  Copyright (c) 2012 Alexandre Aybes. All rights reserved.
+//
+
+#import "PSClocksView.h"
+
+@implementation PSClocksView
+
+- (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
